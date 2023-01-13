ec38e1554837319571a1c041c081f936dae7b966
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-01-13 01:41:03 +0100

    Update ArduinoKitFraschetta.h
    aggiornamento dell'oggetto bluetooth

--- ArduinoKitFraschetta.h
+++ ArduinoKitFraschetta.h
@@ -950,12 +950,14 @@ void FUNZIONE_CREPUSCOLARE(){
 };
 
 class BLUETOOTH{
+private:
+String S;
 public:
 BLUETOOTH(uint32_t baud_rate){Serial.begin(baud_rate);}
-String LEGGI_STRINGA(){
- String S;
- if(Serial.available()){S = Serial.readString();}
- return S;
+String LEGGI_STRINGA(){return S;}
+void RICEVI_STRINGA(){
+if(Serial.available()){S = Serial.readString();}
+else{S="";}
 }
 void INVIA_STRINGA(String S){Serial.print(S);}
 void INVIA_STRINGA_ANDANDO_A_CAPO(String S){Serial.println(S);}
