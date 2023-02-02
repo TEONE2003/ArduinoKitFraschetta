f150917585ea09dab784561477e0cca8d7b91c7a
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-02-02 17:49:02 +0100

    risoluzione bug

--- ArduinoKitFraschetta.h
+++ ArduinoKitFraschetta.h
@@ -1023,25 +1023,25 @@ class LSBDN{//LEGGI SCRIVI BYTE DEI NUMERI
       for(uint8_t p; p<=7; p++){bitWrite(NUM,p,bitRead(B,p));}
      }
      else if(NUM_BYTE == 1){
-      for(uint8_t p=8,b; p<=15; b++,p++){bitWrite(NUM,p,bitRead(B,p));}
+      for(uint8_t p=8,b; p<=15; b++,p++){bitWrite(NUM,p,bitRead(B,b));}
      }
      else if(NUM_BYTE == 2){
-      for(uint8_t p=16,b; p<=23; b++,p++){bitWrite(NUM,p,bitRead(B,p));}
+      for(uint8_t p=16,b; p<=23; b++,p++){bitWrite(NUM,p,bitRead(B,b));}
      }
      else if(NUM_BYTE == 3){
-      for(uint8_t p=24,b; p<=31; b++,p++){bitWrite(NUM,p,bitRead(B,p));}
+      for(uint8_t p=24,b; p<=31; b++,p++){bitWrite(NUM,p,bitRead(B,b));}
      }
      else if(NUM_BYTE == 4){
-      for(uint8_t p=32,b; p<=39; b++,p++){bitWrite(NUM,p,bitRead(B,p));}
+      for(uint8_t p=32,b; p<=39; b++,p++){bitWrite(NUM,p,bitRead(B,b));}
      }
      else if(NUM_BYTE == 5){
-      for(uint8_t p=40,b; p<=47; b++,p++){bitWrite(NUM,p,bitRead(B,p));}
+      for(uint8_t p=40,b; p<=47; b++,p++){bitWrite(NUM,p,bitRead(B,b));}
      }
      else if(NUM_BYTE == 6){
-      for(uint8_t p=48,b; p<=55; b++,p++){bitWrite(NUM,p,bitRead(B,p));}
+      for(uint8_t p=48,b; p<=55; b++,p++){bitWrite(NUM,p,bitRead(B,b));}
      }
      else if(NUM_BYTE == 7){
-      for(uint8_t p=56,b; p<=63; b++,p++){bitWrite(NUM,p,bitRead(B,p));}
+      for(uint8_t p=56,b; p<=63; b++,p++){bitWrite(NUM,p,bitRead(B,b));}
      }
     }
 };
