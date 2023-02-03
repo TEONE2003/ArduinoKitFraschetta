1700d5db7b7920fa07193a0920bd53208073719c
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-02-03 18:01:49 +0100

    debug, risoluzione bug e ottimizzazione

--- examples/ESEMPIO USO MOTORE_STEPPER_BIPOLARE/ESEMPIO USO MOTORE_STEPPER_BIPOLARE.ino
+++ examples/ESEMPIO USO MOTORE_STEPPER_BIPOLARE/ESEMPIO USO MOTORE_STEPPER_BIPOLARE.ino
@@ -1,11 +1,8 @@
 #include "ArduinoKitFraschetta.h"
-MOTORE_STEPPER_BIPOLARE M(2,3,4,5,1024,5);
+MOTORE_STEPPER_BIPOLARE M(2,3,4,5,200,5);
 void setup(){
-M.PASSI(1024);
+
 }
 void loop(){
-delay(1000);
-M.SWIPE_AVANTI();
-delay(1000);
-M.SWIPE_INDIETRO();
+M.PASSO_A_SINISTRA();
 }
