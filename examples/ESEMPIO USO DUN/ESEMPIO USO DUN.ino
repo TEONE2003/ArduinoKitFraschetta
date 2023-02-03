ac3d3c4cfd389c3c57a77cf41bb6cebd4e9c88c8
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-02-03 16:48:18 +0100

    RISOLUZIONE BUG

--- examples/ESEMPIO USO DUN/ESEMPIO USO DUN.ino
+++ examples/ESEMPIO USO DUN/ESEMPIO USO DUN.ino
@@ -4,8 +4,13 @@ uint32_t N=5; //su arduino non funziona la scrittura nei numeri a 64 bit con bit
 uint8_t A[4];
 void setup(){
 DUN::DIVIDI_IN_QUATTRO_UINT32_T(N,A);
+
 for(uint8_t n=0;n<=3; n++) {
-S.INVIA_ANDANDO_A_CAPO(A[n]);
+S.INVIA(String(n)+":"+String(A[n])+" ");
 }
+S.INVIA("\n");
+
+uint32_t N2=DUN::UNISCI_UINT32_T(A[0],A[1],A[2],A[3]);
+S.INVIA_ANDANDO_A_CAPO(String("UNITO:")+String(N2));
 }
 void loop(){}
