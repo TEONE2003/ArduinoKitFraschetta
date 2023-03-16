2ef89bf76038847edfe1f1a20e2a2b325ad44312
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-03-16 11:30:14 +0100

    traduzione in inglese di VirtualCycleFraschetta

--- VirtualCycleFraschetta/VirtualCycleFraschetta.h
+++ VirtualCycleFraschetta/VirtualCycleFraschetta.h
@@ -1,16 +1,16 @@
 #ifndef VirtualCycleFraschetta_h
 #define VirtualCycleFraschetta_h
 #include <VirtualStopwatchFraschetta.h>
-class CicloVirtualeF:CronometroVirtualeF{
-protected: TEMPO Periodo; uint64_t Div;
+class VirtualCycleF:VirtualStopwatchF{
+protected: TimeF Period; uint64_t Divider;
 public:
-    CicloVirtualeF(uint64_t Periodo=0,UNITA_DI_TEMPO Unita=MILLISECONDI):CronometroVirtualeF(UNITA){this->Periodo=TEMPO(Periodo,Unita);}
- TEMPO tempoPassato(){return CronometroVirtualeF::tempoPassato();}
+    VirtualCycleF(uint64_t Period=0,UNITA_DI_TimeF Unit=MILLISECONDI):VirtualStopwatchF(Unit){this->Period=TimeF(Period,Unit);}
+ TimeF ElapsedTime(){return VirtualStopwatchF::ElapsedTime();}
  uint64_t nTick(){
-  avvia();
-  Div=tempoPassato()/Periodo;
-  if(Div>=1){stoppa();}
-  return Div;
+  Start();
+  Divider=ElapsedTime()/Period;
+  if(Divider>=1){Stop();}
+  return Divider;
  }
 };
 #endif
