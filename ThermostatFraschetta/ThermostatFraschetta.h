e2054ec96e71ef59fd509d8d0fb33b6f7660f2a2
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-03-16 09:07:11 +0100

    traduzione in inglese di ThermostatFraschetta

--- ThermostatFraschetta/ThermostatFraschetta.h
+++ ThermostatFraschetta/ThermostatFraschetta.h
@@ -2,19 +2,19 @@
 #define ThermostatFraschetta_h
 #include <ReleFraschetta.h>
 #include <VirtualCycleFraschetta.h>
-enum ThermostatType:boolean{RISCALDAMENTO=1,RAFFREDDAMENTO=0};
-class Thermostat:Rele,CicloVirtualeF{
+enum ThermostatType:boolean{Heating=1,Cooling=0};
+class ThermostatF:ReleF,VirtualCycleF{
  protected:
   ThermostatType TypeT;
  public:
-  uint8_t SOGLIA_TEMPERATURA=20;
- Thermostat():Rele(){}
- Thermostat(uint8_t PinRele,TIPO_DIGITALE TIPORele,ThermostatType TypeT):Rele(PinRele,TIPORele),
- CicloVirtualeF(10,SECONDI){this->TypeT=TypeT;}
- void FUNZIONE_Thermostat(uint8_t TEMPERATURA_ATTUALE){
-  if(TICK()){
-   if(RISCALDAMENTO){if(TEMPERATURA_ATTUALE<SOGLIA_TEMPERATURA){ACCENDI();} else{SPEGNI();}}
-   else{if(TEMPERATURA_ATTUALE>SOGLIA_TEMPERATURA){ACCENDI();} else {SPEGNI();}}
+  uint8_t TemperatureThreshold=20;
+ ThermostatF():ReleF(){}
+ ThermostatF(uint8_t PinRele=0,DigitalType ReleType=NormalLogic,ThermostatType TypeT=Heating):ReleF(PinRele,ReleType),
+ VirtualCycleF(10,Seconds){this->TypeT=TypeT;}
+ void ThermostatFunction(uint8_t CurrentTemperature){
+  if(Tick()){
+   if(Heating){if(CurrentTemperature<TemperatureThreshold){TurnOn();} else{TurnOff();}}
+   else{if(CurrentTemperature>TemperatureThreshold){TurnOn();} else {TurnOff();}}
   }
 }
 };
