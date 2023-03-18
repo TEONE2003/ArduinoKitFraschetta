7224d57cfb4b24f5eebbf0a5afe029ae7c9643ce
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-03-18 11:07:34 +0100

    risoluzione bug e aggiunta di funzioni

--- ThermostatFraschetta.h
+++ ThermostatFraschetta.h
@@ -2,18 +2,20 @@
 #define ThermostatFraschetta_h
 #include "RelayFraschetta.h"
 #include "VirtualCycleFraschetta.h"
-enum ThermostatType:boolean{Heating=1,Cooling=0};
+enum ThermostatType{Heating,Cooling};
 class ThermostatF:RelayF,VirtualCycleF{
- protected:
-  ThermostatType TypeT;
- public:
-  uint8_t TemperatureThreshold=20;
+ protected: ThermostatType TypeT;
+ public: uint8_t TemperatureThreshold;
  ThermostatF(){}
  ThermostatF(uint8_t PinRele,DigitalType ReleType=NormalLogic,ThermostatType TypeT=Heating):RelayF(PinRele,ReleType),
- VirtualCycleF(10,Seconds){this->TypeT=TypeT;}
+ VirtualCycleF(5,Seconds){this->TypeT=TypeT; TemperatureThreshold=EEPROM.read(PinRele);}
+ void SetTemperatureThreshold(uint8_t TemperatureThreshold){
+  EEPROM.update(PinRele,TemperatureThreshold);
+  this->TemperatureThreshold=TemperatureThreshold;
+ }
  void ThermostatFunction(uint8_t CurrentTemperature){
   if(nTick()){
-   if(Heating){if(CurrentTemperature<TemperatureThreshold){TurnOn();} else{TurnOff();}}
+   if(TypeT==Heating){if(CurrentTemperature<TemperatureThreshold){TurnOn();} else{TurnOff();}}
    else{if(CurrentTemperature>TemperatureThreshold){TurnOn();} else {TurnOff();}}
   }
 }
