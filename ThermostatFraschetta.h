f67fa213a8f6754d72649af0b6e46515bff74397
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-03-18 12:17:18 +0100

    correzione bug

--- ThermostatFraschetta.h
+++ ThermostatFraschetta.h
@@ -4,17 +4,17 @@
 #include "VirtualCycleFraschetta.h"
 enum ThermostatType{Heating,Cooling};
 class ThermostatF:RelayF,VirtualCycleF{
- protected: ThermostatType TypeT; uint8_t PinRele,TemperatureThresholdS;
+ protected: ThermostatType TypeT; uint8_t PinRele; byte TemperatureThresholdS;
  public:
  ThermostatF(){}
  ThermostatF(uint8_t PinRele,DigitalType ReleType=NormalLogic,ThermostatType TypeT=Heating):RelayF(PinRele,ReleType),
  VirtualCycleF(5,Seconds){this->TypeT=TypeT; this->PinRele=PinRele; TemperatureThresholdS=EEPROM.read(PinRele);}
- uint8_t TemperatureThreshold(){return TemperatureThresholdS;}
- void SetTemperatureThreshold(uint8_t TemperatureThreshold){
+ byte TemperatureThreshold(){return TemperatureThresholdS;}
+ void SetTemperatureThreshold(byte TemperatureThreshold){
   EEPROM.update(PinRele,TemperatureThreshold);
   TemperatureThresholdS=TemperatureThreshold;
  }
- void ThermostatFunction(uint8_t CurrentTemperature){
+ void ThermostatFunction(byte CurrentTemperature){
   if(nTick()){
    if(TypeT==Heating){if(CurrentTemperature<TemperatureThresholdS){TurnOn();} else{TurnOff();}}
    else{if(CurrentTemperature>TemperatureThresholdS){TurnOn();} else {TurnOff();}}
