4f69f1078e1ca5e2e23e01f942320cc7049fd20c
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-03-18 10:30:30 +0100

    correzione bug

--- VirtualStopwatchFraschetta.h
+++ VirtualStopwatchFraschetta.h
@@ -1,21 +1,21 @@
 #ifndef VirtualStopwatchFraschetta_h
 #define VirtualStopwatchFraschetta_h
-#include <TimeFraschetta.h>
+#include "TimeFraschetta.h"
 class VirtualStopwatchF{
 protected:
 boolean ActiveStopwatch;
-uint64_t CurrentMicroseconds;
-TimeF It,ET;
+uint64_t ST;
+TimeF ET;
 UnitOfTime StopwatchUnitOfTime;
 public:
 void SetUnitOfTime(UnitOfTime Unit){StopwatchUnitOfTime = Unit;}
-VirtualStopwatchF(UnitOfTime Unit=Milliseconds){SetUnitOfTime(Unit); It = TimeF(0,Microseconds); ET=It; CurrentMicroseconds=0; ActiveStopwatch=0;}
-void Start(){if(!ActiveStopwatch){ActiveStopwatch=1; It.Value = micros();}}
+VirtualStopwatchF(UnitOfTime Unit=Milliseconds){SetUnitOfTime(Unit); ST=0; ET=ST; ActiveStopwatch=0;}
+void Start(){if(!ActiveStopwatch){ActiveStopwatch=1; ST = micros();}}
 void Stop(){ActiveStopwatch=0;}
 TimeF ElapsedTime(){
 if(ActiveStopwatch){
-if(It.Value>=CurrentMicroseconds){ET = TimeF(CurrentMicroseconds-It.Value,Microseconds);}
-else{ET=TimeF(0xFFFFFFFFFFFFFFFF-It.Value,Microseconds);}
+if(ST<=micros()){ET = TimeF(micros()-ST,Microseconds);}
+else{ET=TimeF(0xFFFFFFFFFFFFFFFF-ST,Microseconds);}
 return ET.ConvertedTo(StopwatchUnitOfTime);
 }
 else{return TimeF(0,StopwatchUnitOfTime);}
--- examples/VirtualCycleFraschetta_example/VirtualCycleFraschetta_example.ino
+++ examples/VirtualCycleFraschetta_example/VirtualCycleFraschetta_example.ino
@@ -1,6 +1,4 @@
-#include <LedFraschetta.h>
 #include <VirtualCycleFraschetta.h>
 VirtualCycleF C(2,Seconds);
-LedF L(13);
-void setup(){}
-void loop(){for(uint64_t n=C.nTick();n>=1;n--){L.InvertStatus();}}
+void setup(){pinMode(13,OUTPUT);}
+void loop(){for(uint64_t n=C.nTick();n>=1;n--){digitalWrite(13,!digitalRead(13));}}
