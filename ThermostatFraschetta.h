2cd992cc08ceea260a697a8edb2bb36bf67155f7
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-03-18 11:29:02 +0100

    risoluzione bug

--- ThermostatFraschetta.h
+++ ThermostatFraschetta.h
@@ -4,20 +4,20 @@
 #include "VirtualCycleFraschetta.h"
 enum ThermostatType{Heating,Cooling};
 class ThermostatF:RelayF,VirtualCycleF{
- protected: ThermostatType TypeT; uint8_t PinRele,TemperatureThreshold;
+ protected: ThermostatType TypeT; uint8_t PinRele,TemperatureThresholdS;
  public:
  ThermostatF(){}
  ThermostatF(uint8_t PinRele,DigitalType ReleType=NormalLogic,ThermostatType TypeT=Heating):RelayF(PinRele,ReleType),
- VirtualCycleF(5,Seconds){this->TypeT=TypeT; this->PinRele=PinRele; TemperatureThreshold=EEPROM.read(PinRele);}
- uint8_t TemperatureThreshold(){return TemperatureThreshold;}
+ VirtualCycleF(5,Seconds){this->TypeT=TypeT; this->PinRele=PinRele; TemperatureThresholdS=EEPROM.read(PinRele);}
+ uint8_t TemperatureThreshold(){return TemperatureThresholdS;}
  void SetTemperatureThreshold(uint8_t TemperatureThreshold){
   EEPROM.update(PinRele,TemperatureThreshold);
-  this->TemperatureThreshold=TemperatureThreshold;
+  TemperatureThresholdS=TemperatureThreshold;
  }
  void ThermostatFunction(uint8_t CurrentTemperature){
   if(nTick()){
-   if(TypeT==Heating){if(CurrentTemperature<TemperatureThreshold){TurnOn();} else{TurnOff();}}
-   else{if(CurrentTemperature>TemperatureThreshold){TurnOn();} else {TurnOff();}}
+   if(TypeT==Heating){if(CurrentTemperature<TemperatureThresholdS){TurnOn();} else{TurnOff();}}
+   else{if(CurrentTemperature>TemperatureThresholdS){TurnOn();} else {TurnOff();}}
   }
 }
 };
