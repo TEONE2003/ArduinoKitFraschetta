53c46912e67a5484f9b48b60a3fcf7c13b39bae4
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-03-27 14:40:00 +0200

    Revert "correzione bug e creazione di BasicSerialPhotocellDuskToDawnFraschetta"
    This reverts commit ef09bd464ff198eb1ecf092a3506d6c0ceb00e03.

--- BasicSerialRelayFraschetta.h
+++ BasicSerialRelayFraschetta.h
@@ -7,7 +7,6 @@ public:
  BasicSerialRelayF(uint8_t Pin,String Tag,DigitalType Type=NormalLogic,MemorizeStatus Save=DoNotSaveStatus,String StatusRequestString=""):RelayF(Pin,Type,Save){
   TurnOnString=Tag+'1'; TurnOffString=Tag+'0';
   this->StatusRequestString=StatusRequestString;
-  invalidCommand=0;
  }
  bool InvalidCommand(){return invalidCommand;}
  void SendState(){
