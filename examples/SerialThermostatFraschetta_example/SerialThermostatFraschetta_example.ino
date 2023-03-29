7486a2ad4b601e76ce0aa95556d47758af3c0eaf
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-03-29 15:29:34 +0200

    correzione bug

--- examples/SerialThermostatFraschetta_example/SerialThermostatFraschetta_example.ino
+++ examples/SerialThermostatFraschetta_example/SerialThermostatFraschetta_example.ino
@@ -5,6 +5,6 @@ BluetoothF B(2,3,9600);
 void setup(){}
 void loop(){
 B.ReceiveString();
-T.SerialThermostatFunction(24,30,B.ReadString());
+T.SerialThermostatFunction(24,B.ReadString());
 if(T.InvalidCommand()){B.SendLn("INVALID COMMAND");}
 }
