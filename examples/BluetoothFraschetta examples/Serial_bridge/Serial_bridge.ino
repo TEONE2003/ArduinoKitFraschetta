913e68896317d7a3fc7fe88f2d90cb995816a41a
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-03-31 14:27:38 +0200

    spiegazione delle impostazioni

--- examples/BluetoothFraschetta examples/Serial_bridge/Serial_bridge.ino
+++ examples/BluetoothFraschetta examples/Serial_bridge/Serial_bridge.ino
@@ -2,3 +2,8 @@
 BluetoothF B(2,3,9600);
 void setup(){}
 void loop(){B.SerialBridgeFunction();}
+/*
+Set up your serial monitor with the following settings:
+baud: 9600
+Line Ending Characters: Both NL & CR
+/*
