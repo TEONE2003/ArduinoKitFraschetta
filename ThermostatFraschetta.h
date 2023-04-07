3a02f71702b95e4b3f1bb069006f5294c595fd85
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-04-07 13:44:05 +0200

    correzione bug

--- ThermostatFraschetta.h
+++ ThermostatFraschetta.h
@@ -8,22 +8,24 @@ protected: ThermostatType TypeT; uint8_t PinRelay; byte TemperatureThresholdS; b
  public:
  ThermostatF(){}
  ThermostatF(uint8_t PinRelay,DigitalType ReleType=NormalLogic,ThermostatType TypeT=Heating):RelayF(PinRelay,ReleType),
-    VirtualCycleF(5,Seconds){State=1; this->TypeT=TypeT; this->PinRelay=PinRelay; TemperatureThresholdS=0;}
+    VirtualCycleF(5,Seconds){State=0; this->TypeT=TypeT; this->PinRelay=PinRelay; TemperatureThresholdS=0;}
  byte TemperatureThreshold(){return TemperatureThresholdS;}
  using RelayF::ReadDigitalStatus;
  void Begin(){
   RelayF::Begin();
-  if(EEPROM.read(PinRelay)>49){EEPROM.update(PinRelay,20);}
+  if(EEPROM.read(PinRelay)>49){EEPROM.update(PinRelay,-20);}
   TemperatureThresholdS=EEPROM.read(PinRelay);
+  if(TemperatureThresholdS<0){TemperatureThresholdS=-TemperatureThresholdS;}else{State=1;}
  }
  void SetTemperatureThreshold(byte TemperatureThreshold){
-  EEPROM.update(PinRelay,TemperatureThreshold);
   TemperatureThresholdS=TemperatureThreshold;
+  if(!State){TemperatureThreshold=-TemperatureThreshold;}
+  EEPROM.update(PinRelay,TemperatureThreshold);
  }
- void TurnOn(){State=1; /*if(TemperatureThresholdS<0){SetTemperatureThreshold(-TemperatureThresholdS);}*/}
- void TurnOff(){State=0; /*if(TemperatureThresholdS>=0){SetTemperatureThreshold(-TemperatureThresholdS);}*/}
+ void TurnOn(){State=1; SetTemperatureThreshold(TemperatureThresholdS);}
+ void TurnOff(){State=0; SetTemperatureThreshold(TemperatureThresholdS);}
  void ThermostatFunction(byte CurrentTemperature){
-  if(nTick() && State){
+  if(nTick()>0 && State){
    if(TypeT==Heating){if(CurrentTemperature<TemperatureThresholdS){RelayF::TurnOn();} else{RelayF::TurnOff();}}
    else{if(CurrentTemperature>TemperatureThresholdS){RelayF::TurnOn();} else {RelayF::TurnOff();}}
   }
