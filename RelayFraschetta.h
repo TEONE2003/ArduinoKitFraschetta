98ce3e44e18a97548beb76cfd0c1f0bd2bbf9d1f
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-04-07 16:48:30 +0200

    CORREZIONE BUG

--- RelayFraschetta.h
+++ RelayFraschetta.h
@@ -3,7 +3,8 @@
 #include "OutputFraschetta.h"
 class RelayF:OutputF{
  public:
-  RelayF(uint8_t Pin=0,DigitalType Type=NormalLogic,MemorizeStatus Save=DoNotSaveStatus):OutputF(Pin,Type,Save){}
+  RelayF(){}
+  RelayF(uint8_t Pin,DigitalType Type=NormalLogic,MemorizeStatus Save=DoNotSaveStatus):OutputF(Pin,Type,Save){}
     using OutputF::Begin; using OutputF::TurnOn; using OutputF::TurnOff; using OutputF::InvertStatus; using OutputF::Blink; using OutputF::ReadDigitalStatus;
 };
 #endif
