1141ca6a3a7a8df650213ca850dcca232e2a8ca5
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-04-17 11:29:36 +0200

    correzione bug

--- VirtualCycleFraschetta.h
+++ VirtualCycleFraschetta.h
@@ -4,7 +4,7 @@
 class VirtualCycleF:VirtualStopwatchF{
 protected: TimeF Period; uint64_t Divider; bool I;
 public:
- VirtualCycleF(uint64_t Period=0,UnitOfTime Unit=Seconds):VirtualStopwatchF(Unit){I=1; this->Period=TimeF(Period,Unit);}
+ VirtualCycleF(uint64_t Period=0,UnitOfTime Unit=Seconds):VirtualStopwatchF(Unit){I=1; Divider=0; this->Period=TimeF(Period,Unit);}
  bool Inizialized(){return I;}
  TimeF ElapsedTime(){return VirtualStopwatchF::ElapsedTime();}
  uint64_t nTick(){
