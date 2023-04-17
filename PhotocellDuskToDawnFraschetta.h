83f72b24289970241fe10a24e60dec3ffe7be4b1
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-04-17 17:47:16 +0200

    aggiunta funzione di bypass in PhotocellDuskToDawnF

--- PhotocellDuskToDawnFraschetta.h
+++ PhotocellDuskToDawnFraschetta.h
@@ -3,11 +3,14 @@
 #include "RelayFraschetta.h"
 #include "PhotoresistorFraschetta.h"
 class PhotocellDuskToDawnF:RelayF,PhotoresistorF{
+private: bool Bypass;
 public:
  PhotocellDuskToDawnF(uint8_t PinPhotoresistor,uint16_t TurnOnThreshold,uint16_t TurnOffThreshold,uint64_t Delay,UnitOfTime Unit,
  uint8_t PinRele,DigitalType ReleType):PhotoresistorF(PinPhotoresistor,TurnOnThreshold,TurnOffThreshold,Delay,Unit),
- RelayF(PinRele,ReleType){}
- void PhotocellDuskToDawnFunction(){if(Check()){TurnOn();} else{TurnOff();}}
+ RelayF(PinRele,ReleType){Bypass=0;}
+ void PhotocellDuskToDawnFunction(){if(!Bypass){if(Check()){TurnOn();} else{TurnOff();}}}
+ void BypassOn(){Bypass=1; TurnOn();}
+ void BypassOff(){Bypass=0;}
  using RelayF::ReadDigitalStatus;
  void Begin(){RelayF::Begin(); PhotoresistorF::Begin();}
 };
