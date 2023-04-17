f85b3ba63c2db83430e2ef986005655e50c92234
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-04-17 14:45:16 +0200

    CORREZIONE BUG

--- DhtFraschetta.h
+++ DhtFraschetta.h
@@ -2,10 +2,10 @@
 #define DhtFraschetta_h
 #include "DHT.h"
 class DhtF:DHT{
- protected: bool Initialized; void InitializesIfNotInitialized(){if(!Initialized){begin();}}
+ protected: bool I; void InitializesIfNotI(){if(!I){begin(); I=1;}}
  public:
-  DhtF(uint8_t Pin,uint8_t Type):DHT(Pin,Type){}
-  float ReadTemperature(){InitializesIfNotInitialized(); return readTemperature();}
-  float ReadHumidity(){InitializesIfNotInitialized(); return readHumidity();}
+  DhtF(uint8_t Pin,uint8_t Type):DHT(Pin,Type){I=0;}
+  float ReadTemperature(){InitializesIfNotI(); return readTemperature();}
+  float ReadHumidity(){InitializesIfNotI(); return readHumidity();}
 };
 #endif
