a83be5451a2742674b42a6c59f365c9e83668b71
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-04-18 09:53:00 +0200

    agginta funzione di begin in DhtF per evitare possibili errori

--- DhtFraschetta.h
+++ DhtFraschetta.h
@@ -2,13 +2,13 @@
 #define DhtFraschetta_h
 #include "DHT.h"
 class DhtF:DHT{
- private: bool I; float PreviousT,PreviousH;
- void InitializesIfNotI(){if(!I){begin(); I=1; PreviousT=readTemperature(); PreviousH=readHumidity();}}
+ private: float PreviousT,PreviousH;
  public:
-  DhtF(uint8_t Pin,uint8_t Type):DHT(Pin,Type){I=0; PreviousT=0; PreviousH=0;}
-  float ReadTemperature(){InitializesIfNotI(); return readTemperature();}
-  float ReadHumidity(){InitializesIfNotI(); return readHumidity();}
-  bool TemperatureChanged(){InitializesIfNotI(); if(PreviousT!=readTemperature()){PreviousT=readTemperature(); return 1;}return 0;}
-  bool HumidityChanged(){InitializesIfNotI(); if(PreviousH!=readHumidity()){PreviousH=readHumidity(); return 1;}return 0;}
+  DhtF(uint8_t Pin,uint8_t Type):DHT(Pin,Type){PreviousT=0; PreviousH=0;}
+  void Begin(){begin(); PreviousT=readTemperature(); PreviousH=readHumidity();}
+  float ReadTemperature(){return readTemperature();}
+  float ReadHumidity(){return readHumidity();}
+  bool TemperatureChanged(){if(PreviousT!=readTemperature()){PreviousT=readTemperature(); return 1;}return 0;}
+  bool HumidityChanged(){if(PreviousH!=readHumidity()){PreviousH=readHumidity(); return 1;}return 0;}
 };
 #endif
