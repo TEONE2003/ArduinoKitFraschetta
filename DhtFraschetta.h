012f85913d6a987fe5569b1bbd26798ec05ef848
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-04-18 09:46:23 +0200

    correzione bug

--- DhtFraschetta.h
+++ DhtFraschetta.h
@@ -2,12 +2,13 @@
 #define DhtFraschetta_h
 #include "DHT.h"
 class DhtF:DHT{
- private: bool I; float PreviousT,PreviousH; void InitializesIfNotI(){if(!I){begin(); I=1;}}
+ private: bool I; float PreviousT,PreviousH;
+ void InitializesIfNotI(){if(!I){begin(); I=1; PreviousT=readTemperature(); PreviousH=readHumidity();}}
  public:
   DhtF(uint8_t Pin,uint8_t Type):DHT(Pin,Type){I=0; PreviousT=0; PreviousH=0;}
   float ReadTemperature(){InitializesIfNotI(); return readTemperature();}
   float ReadHumidity(){InitializesIfNotI(); return readHumidity();}
-  bool TemperatureChanged(){if(PreviousT!=readTemperature()){PreviousT=readTemperature(); return 1;}return 0;}
-  bool HumidityChanged(){if(PreviousH!=readHumidity()){PreviousH=readHumidity(); return 1;}return 0;}
+  bool TemperatureChanged(){InitializesIfNotI(); if(PreviousT!=readTemperature()){PreviousT=readTemperature(); return 1;}return 0;}
+  bool HumidityChanged(){InitializesIfNotI(); if(PreviousH!=readHumidity()){PreviousH=readHumidity(); return 1;}return 0;}
 };
 #endif
