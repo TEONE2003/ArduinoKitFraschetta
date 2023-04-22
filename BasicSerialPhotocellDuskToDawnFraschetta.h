463bc96054e5ed804444bd6e7ba4ee58e2d8836a
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-04-22 18:20:36 +0200

    aggiunta funzioni per sapere lo stato della luce senza  guardare il rele

--- BasicSerialPhotocellDuskToDawnFraschetta.h
+++ BasicSerialPhotocellDuskToDawnFraschetta.h
@@ -4,24 +4,27 @@
 #include "SerialDriverObjectFraschetta.h"
 class BasicSerialPhotocellDuskToDawnF:public SerialDriverObjectF,public PhotocellDuskToDawnF{
 protected:
- virtual void Send(String s)=0; String StatusRequestString,BypassString,InvertBypassString,BypassOnString,BypassOffString;
+ virtual void Send(String s)=0; String RelayStatusString,BypassString,InvertBypassString,BypassOnString,BypassOffString,LightRequest;
  using PhotocellDuskToDawnF::PhotocellDuskToDawnFunction;
  BasicSerialPhotocellDuskToDawnF(String Tag,uint8_t PinPhotoresistor,uint16_t TurnOnThreshold,uint16_t TurnOffThreshold,uint64_t Delay,UnitOfTime Unit,
                       uint8_t PinRele,DigitalType ReleType):PhotocellDuskToDawnF(PinPhotoresistor,TurnOnThreshold,TurnOffThreshold,Delay,Unit,PinRele,ReleType),SerialDriverObjectF(){
-     StatusRequestString=Tag+".S"; BypassString=Tag+".B"; InvertBypassString=Tag+".IB"; BypassOnString=Tag+".B=1"; BypassOffString=Tag+".B=0";
+     RelayStatusString=Tag+".S"; BypassString=Tag+".B"; InvertBypassString=Tag+".IB"; BypassOnString=Tag+".B=1"; BypassOffString=Tag+".B=0"; LightRequest=Tag+".L";
  }
 public:
-    void SendStatus(){if(ReadDigitalStatus()){Send(StatusRequestString+"=1");}else{Send(StatusRequestString+"=0");}}
-    void SendStatusBypass(){if(Bypass){Send(BypassOnString);}else{Send(BypassOffString);}}
+    void SendRelayStatus(){if(ReadDigitalStatus()){Send(RelayStatusString+"=1");}else{Send(RelayStatusString+"=0");}}
+    void SendBypassStatus(){if(Bypass){Send(BypassOnString);}else{Send(BypassOffString);}}
+    void SendLightStatus(){Send(LightRequest+"="+String(StatusLight()));}
     void SerialPhotocellDuskToDawnFunction(String ReceivedString){
         PhotocellDuskToDawnFunction(); SetReceivedString(ReceivedString);
-        if(CommandFound(StatusRequestString)){SendStatus();}
-        else if(CommandFound(BypassString)){SendStatusBypass();}
-        else if(CommandFound(InvertBypassString)){InvertBypass(); SendStatusBypass();}
-        else if(CommandFound(BypassOnString)){BypassOn(); SendStatusBypass();}
-        else if(CommandFound(BypassOffString)){BypassOff(); SendStatusBypass();}
+        if(CommandFound(RelayStatusString)){SendRelayStatus();}
+        else if(CommandFound(LightRequest)){SendLightStatus();}
+        else if(CommandFound(BypassString)){SendBypassStatus();}
+        else if(CommandFound(InvertBypassString)){InvertBypass(); SendBypassStatus();}
+        else if(CommandFound(BypassOnString)){BypassOn(); SendBypassStatus();}
+        else if(CommandFound(BypassOffString)){BypassOff(); SendBypassStatus();}
         else{SetInvalidCommand();}
-        if(RelayChanged()){SendStatus();}
+        if(LightChanged()){SendLightStatus();}
+        if(RelayChanged()){SendRelayStatus();}
     }
 };
 #endif
--- PhotocellDuskToDawnFraschetta.h
+++ PhotocellDuskToDawnFraschetta.h
@@ -3,18 +3,20 @@
 #include "RelayFraschetta.h"
 #include "PhotoresistorFraschetta.h"
 class PhotocellDuskToDawnF:RelayF,PhotoresistorF{
-protected: bool Bypass,PreviousStatus;
+protected: bool Bypass,PreviousRelayStatus,PreviousLightStatus;
 public:
  PhotocellDuskToDawnF(uint8_t PinPhotoresistor,uint16_t TurnOnThreshold,uint16_t TurnOffThreshold,uint64_t Delay,UnitOfTime Unit,
  uint8_t PinRele,DigitalType ReleType):PhotoresistorF(PinPhotoresistor,TurnOnThreshold,TurnOffThreshold,Delay,Unit),
- RelayF(PinRele,ReleType){Bypass=0; PreviousStatus=0;}
+ RelayF(PinRele,ReleType){Bypass=0; PreviousRelayStatus=0; PreviousLightStatus=0;}
  void PhotocellDuskToDawnFunction(){if(!Bypass){if(Check()){TurnOn();} else{TurnOff();}}}
+ bool StatusLight(){return Check();}
+ bool LightChanged(){if(Check()!=PreviousLightStatus){PreviousLightStatus=Check(); return 1;}return 0;}
  void BypassOn(){Bypass=1; TurnOn();}
  void BypassOff(){Bypass=0;}
  void InvertBypass(){if(Bypass){Bypass=0;} else{BypassOn();}}
  using RelayF::ReadDigitalStatus;
- bool RelayChanged(){if(ReadDigitalStatus()!=PreviousStatus){PreviousStatus=ReadDigitalStatus(); return 1;} return 0;}
- void Begin(){RelayF::Begin(); PhotoresistorF::Begin();}
+ bool RelayChanged(){if(ReadDigitalStatus()!=PreviousRelayStatus){PreviousRelayStatus=ReadDigitalStatus(); return 1;} return 0;}
+ void Begin(){RelayF::Begin(); PhotoresistorF::Begin(); PreviousLightStatus=Check(); PreviousRelayStatus=ReadDigitalStatus();}
 };
 #endif
 
