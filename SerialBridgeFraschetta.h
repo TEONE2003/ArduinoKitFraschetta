e8c49d76919d5e2b05018bed5105b46a5d9fc852
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-04-28 17:30:33 +0200

    correzione bug

--- SerialBridgeFraschetta.h
+++ SerialBridgeFraschetta.h
@@ -2,7 +2,7 @@
 #define SerialBridgeFraschetta_h
 class SerialBridgeF{
 private:
- bool SerialBridge,long Baud;
+ bool SerialBridge; long Baud;
  virtual uint8_t SerialRead()=0;
  virtual void SerialWrite(uint8_t B)=0;
  virtual bool DataPresent()=0;
@@ -10,7 +10,7 @@ protected: SerialBridgeF(long Baud=9600){this->Baud=Baud; SerialBridge=0;}
 public:
 void SerialBridgeFunction(){
   if(!SerialBridge){Serial.end(); Serial.begin(Baud); SerialBridge=1;}
-  if(Serial.available>0){SerialWrite(Serial.read());}
+  if(Serial.available()>0){SerialWrite(Serial.read());}
   if(DataPresent()){Serial.write(SerialRead());}
 }
 };
