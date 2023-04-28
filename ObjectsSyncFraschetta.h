3cd22ddb30ae44cfe4f221fbb64f86624a9099b2
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-04-28 14:42:38 +0200

    correzione bug

--- ObjectsSyncFraschetta.h
+++ ObjectsSyncFraschetta.h
@@ -1,7 +1,7 @@
 #ifndef ObjectsSyncFraschetta_h
 #define ObjectsSyncFraschetta_h
 class ObjectsSyncF{
-private: void void (*SendAllStatus)(void); bool invalidCommand;
+private: void (*SendAllStatus)(void); bool invalidCommand;
 public:
  ObjectsSyncF(void (*SendAllStatus)(void)){this->SendAllStatus=SendAllStatus; invalidCommand=0;}
  void ObjectsSync(String ReceivedString){invalidCommand=0; if(ReceivedString=="SYNC"){SendAllStatus();}else{invalidCommand=1;}}
