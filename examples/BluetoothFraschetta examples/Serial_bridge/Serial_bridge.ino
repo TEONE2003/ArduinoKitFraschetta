03879a1c56193059742ac21d12035a75f5244445
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-05-03 12:04:19 +0200

    CORREZIONE BUG

--- examples/BluetoothFraschetta examples/Serial_bridge/Serial_bridge.ino
+++ examples/BluetoothFraschetta examples/Serial_bridge/Serial_bridge.ino
@@ -1,6 +1,6 @@
 #include "BluetoothFraschetta.h"
-BluetoothF B(2,3,9600); /*it may be necessary to change the speed to 38400, some communicate to 9600 and others to 38400*/
-void setup(){}
+BluetoothF B(5,4,9600); /*it may be necessary to change the speed to 38400, some communicate to 9600 and others to 38400*/
+void setup(){B.Begin();}
 void loop(){B.SerialBridgeFunction();}
 /*
 If you are configuring the bluetooth module then configure your serial monitor with the following settings:
