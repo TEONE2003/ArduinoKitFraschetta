7b1df6a6dc4a9c051553f642ab311cdc08927320
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-05-04 12:31:26 +0200

    ottimizzazione

--- PhotoresistorFraschetta.h
+++ PhotoresistorFraschetta.h
@@ -5,15 +5,15 @@
 class PhotoresistorF:InputF,VirtualCycleF{
 protected:
  uint16_t TurnOnThreshold,TurnOffThreshold;
- bool LastCheckExecuted,SerialInizialized;
+ bool LastCheckExecuted;
 public:
  PhotoresistorF(uint8_t Pin,uint16_t TurnOnThreshold,uint16_t TurnOffThreshold,uint64_t Delay,UnitOfTime Unit):InputF(Pin),VirtualCycleF(Delay,Unit){
   this->TurnOnThreshold = TurnOnThreshold;
   this->TurnOffThreshold = TurnOffThreshold;
-  LastCheckExecuted=0; SerialInizialized=0;
+  LastCheckExecuted=false;
  }
  uint16_t ReadPhotoresistor(){return AnalogRead();}
- void Calibrate(){if(!SerialInizialized){Serial.end(); Serial.begin(9600); SerialInizialized=1;} Serial.print("Photoresistor: "+String(ReadPhotoresistor())+"\n");}
+ void Calibrate(){static bool SerialInizialized=false; if(!SerialInizialized){Serial.end(); Serial.begin(9600); SerialInizialized=true;} Serial.print("Photoresistor: "+String(ReadPhotoresistor())+"\n");}
  boolean Check(){
  if(nTick()){
   if(ReadPhotoresistor()>=TurnOnThreshold){LastCheckExecuted=1; return 1;}
--- SerialBridgeFraschetta.h
+++ SerialBridgeFraschetta.h
@@ -2,14 +2,15 @@
 #define SerialBridgeFraschetta_h
 class SerialBridgeF{
 private:
- bool SerialBridge; long Baud;
+ long Baud;
  virtual uint8_t SerialRead()=0;
  virtual void SerialWrite(uint8_t B)=0;
  virtual bool DataPresent()=0;
-protected: SerialBridgeF(long Baud=9600){this->Baud=Baud; SerialBridge=0;}
+protected: SerialBridgeF(long Baud=9600){this->Baud=Baud;}
 public:
 void SerialBridgeFunction(){
-  if(!SerialBridge){Serial.end(); Serial.begin(Baud); SerialBridge=1;}
+  static bool SerialBridge=false;
+  if(!SerialBridge){Serial.end(); Serial.begin(Baud); SerialBridge=true;}
   if(Serial.available()>0){SerialWrite(Serial.read());}
   if(DataPresent()){Serial.write(SerialRead());}
 }
