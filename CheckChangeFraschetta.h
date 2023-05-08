26cbad318c946d2ab43c6e6e8c29110eaeccff4f
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2023-05-08 23:56:37 +0200

    creato oggetto che controlla se il valore datogli in pasto è cambiato

--- CheckChangeFraschetta.h
+++ CheckChangeFraschetta.h
@@ -1,7 +1,9 @@
-#ifndef AlarmFraschetta_h
-#define AlarmFraschetta_h
-#include "MatrixKeyboardFraschetta.h"
-class AlarmF:MatrixKeyboardF{
-    
+#ifndef CheckChangeFraschetta_h
+#define CheckChangeFraschetta_h
+template <class T> class CheckChangeF{
+ private: T V,Vs;
+ public:
+  CheckChangeF(T &V){this->V=V; Vs=this->V;}
+  bool Changed(){if(Vs!=V){Vs=V; return 1; return 0;}}
 };
 #endif
