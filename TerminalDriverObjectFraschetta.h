d1b561f6f79ef589d1f3bed542d75e54ff0281db
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-01-16 12:32:38 +0100

    Update TerminalDriverObjectFraschetta.h

--- TerminalDriverObjectFraschetta.h
+++ TerminalDriverObjectFraschetta.h
@@ -1,15 +1,14 @@
 #ifndef TerminalDriverObjectFraschetta_h
 #define TerminalDriverObjectFraschetta_h
 class TerminalDriverObjectF{
-private: bool invalidCommand; String ReceivedString; int NIndex;
+private: bool invalidCommand; String *ReceivedString; int NIndex;
 protected:
 static const String SyncCommand;
- void SetReceivedString(String ReceivedString){this->ReceivedString=ReceivedString;}
  void SetInvalidCommand(){if(ReceivedString!=""){invalidCommand=1;}}
  bool CommandFound(String Command){invalidCommand=0; return Command==ReceivedString;}
  bool ContentCommand(String Command){invalidCommand=0; NIndex=ReceivedString.indexOf(Command); return NIndex>=0;}
  int ExtractNumber(int TagLength,uint8_t NumberDigits){NIndex+=TagLength; return ReceivedString.substring(NIndex+1,NIndex+NumberDigits+1).toInt();}
- TerminalDriverObjectF(){invalidCommand=0; ReceivedString=""; NIndex=-1;}
+ TerminalDriverObjectF(String *ReceivedString){invalidCommand=0; this.ReceivedString=ReceivedString; NIndex=-1;}
 public: bool InvalidCommand(){return invalidCommand;}
 };
 const String TerminalDriverObjectF::SyncCommand="SYNC";
