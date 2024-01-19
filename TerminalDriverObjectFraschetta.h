8ef9d3945f60c3eae40b7a5a7d1e9463e4444f17
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-01-19 19:39:40 +0100

    Update TerminalDriverObjectFraschetta.h

--- TerminalDriverObjectFraschetta.h
+++ TerminalDriverObjectFraschetta.h
@@ -1,15 +1,22 @@
 #ifndef TerminalDriverObjectFraschetta_h
 #define TerminalDriverObjectFraschetta_h
 class TerminalDriverObjectF{
-private: bool invalidCommand; String *ReceivedString; int NIndex;
+private: int CountInvalidCommand; String *ReceivedString; int NIndex;
+void SendLn(String Command)=0;
 protected:
 static const String SyncCommand;
- void SetInvalidCommand(){if(ReceivedString!=""){invalidCommand=1;}}
- bool CommandFound(String Command){invalidCommand=0; return Command==ReceivedString;}
- bool ContentCommand(String Command){invalidCommand=0; NIndex=ReceivedString.indexOf(Command); return NIndex>=0;}
+static const int TInstanceCounter;
+ void SetInvalidCommand(){
+  if(ReceivedString!=""){
+   CountInvalidCommand+=1;
+   if(CountInvalidCommand==TInstanceCounter){SendLn("INVALID COMMAND: ["+ReceivedString+"]");}
+  }
+ }
+ bool CommandFound(String Command){CountInvalidCommand-=1; return Command==ReceivedString;}
+ bool ContentCommand(String Command){CountInvalidCommand-=1; NIndex=ReceivedString.indexOf(Command); return NIndex>=0;}
  int ExtractNumber(int TagLength,uint8_t NumberDigits){NIndex+=TagLength; return ReceivedString.substring(NIndex+1,NIndex+NumberDigits+1).toInt();}
- TerminalDriverObjectF(String *ReceivedString){invalidCommand=0; this.ReceivedString=ReceivedString; NIndex=-1;}
-public: bool InvalidCommand(){return invalidCommand;}
+ TerminalDriverObjectF(String *ReceivedString){this->TInstanceCounter+=1; this.ReceivedString=ReceivedString; NIndex=-1;}
 };
 const String TerminalDriverObjectF::SyncCommand="SYNC";
+const int TerminalDriverObjectF::TInstanceCounter=0;
 #endif
