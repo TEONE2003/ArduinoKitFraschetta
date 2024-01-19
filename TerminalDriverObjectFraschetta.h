0716512d1ac4e9f93c8b1fceb2041ac0ad5078a1
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-01-19 19:41:00 +0100

    Update TerminalDriverObjectFraschetta.h

--- TerminalDriverObjectFraschetta.h
+++ TerminalDriverObjectFraschetta.h
@@ -1,22 +1,24 @@
 #ifndef TerminalDriverObjectFraschetta_h
 #define TerminalDriverObjectFraschetta_h
 class TerminalDriverObjectF{
-private: int CountInvalidCommand; String *ReceivedString; int NIndex;
+private: String *ReceivedString; int NIndex; bool Subscribe;
 void SendLn(String Command)=0;
 protected:
 static const String SyncCommand;
-static const int TInstanceCounter;
+static const int InstanceCounter;
+static const int CountInvalidCommand;
  void SetInvalidCommand(){
-  if(ReceivedString!=""){
-   CountInvalidCommand+=1;
-   if(CountInvalidCommand==TInstanceCounter){SendLn("INVALID COMMAND: ["+ReceivedString+"]");}
+  if(*ReceivedString!=""){
+  if(Subscribe){CountInvalidCommand+=1; Subscribe=0;} 
+  if(CountInvalidCommand==InstanceCounter) SendLn("INVALID COMMAND: ["+ *ReceivedString +"]");
   }
  }
- bool CommandFound(String Command){CountInvalidCommand-=1; return Command==ReceivedString;}
- bool ContentCommand(String Command){CountInvalidCommand-=1; NIndex=ReceivedString.indexOf(Command); return NIndex>=0;}
- int ExtractNumber(int TagLength,uint8_t NumberDigits){NIndex+=TagLength; return ReceivedString.substring(NIndex+1,NIndex+NumberDigits+1).toInt();}
- TerminalDriverObjectF(String *ReceivedString){this->TInstanceCounter+=1; this.ReceivedString=ReceivedString; NIndex=-1;}
+ bool CommandFound(String Command){Subscribe=1; return Command == *ReceivedString;}
+ bool ContentCommand(String Command){Subscribe=1; NIndex= *ReceivedString.indexOf(Command); return NIndex>=0;}
+ int ExtractNumber(int TagLength,uint8_t NumberDigits){NIndex+=TagLength; return *ReceivedString.substring(NIndex+1,NIndex+NumberDigits+1).toInt();}
+ TerminalDriverObjectF(String *ReceivedString){this->InstanceCounter+=1; this.ReceivedString=ReceivedString; NIndex=-1; Subscribe=false;}
 };
 const String TerminalDriverObjectF::SyncCommand="SYNC";
-const int TerminalDriverObjectF::TInstanceCounter=0;
+const int TerminalDriverObjectF::InstanceCounter=0;
+const int TerminalDriverObjectF::CountInvalidCommand=0;
 #endif
