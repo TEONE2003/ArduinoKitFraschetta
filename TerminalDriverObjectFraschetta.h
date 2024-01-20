5ab93af5882f98bd26add5ac7f7cc1226144b080
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-01-20 10:25:19 +0100

    Update TerminalDriverObjectFraschetta.h

--- TerminalDriverObjectFraschetta.h
+++ TerminalDriverObjectFraschetta.h
@@ -2,23 +2,25 @@
 #define TerminalDriverObjectFraschetta_h
 class TerminalDriverObjectF{
 private: String *ReceivedString; int NIndex; bool Subscribe;
-void SendLn(String Command)=0;
+virtual void SendLn(String Command)=0;
 protected:
 static const String SyncCommand;
-static const int InstanceCounter;
-static const int CountInvalidCommand;
+static int InstanceCounter;
+static int CountInvalidCommand;
  void SetInvalidCommand(){
-  if(*ReceivedString!=""){
+  if((*ReceivedString)!=""){
   if(Subscribe){CountInvalidCommand+=1; Subscribe=0;} 
-  if(CountInvalidCommand==InstanceCounter) SendLn("INVALID COMMAND: ["+ *ReceivedString +"]");
+  if(CountInvalidCommand==InstanceCounter) SendLn("INVALID COMMAND: ["+ (*ReceivedString) +"]");
   }
  }
  bool CommandFound(String Command){Subscribe=1; return Command == *ReceivedString;}
- bool ContentCommand(String Command){Subscribe=1; NIndex= *ReceivedString.indexOf(Command); return NIndex>=0;}
- int ExtractNumber(int TagLength,uint8_t NumberDigits){NIndex+=TagLength; return *ReceivedString.substring(NIndex+1,NIndex+NumberDigits+1).toInt();}
- TerminalDriverObjectF(String *ReceivedString){this->InstanceCounter+=1; this.ReceivedString=ReceivedString; NIndex=-1; Subscribe=false;}
+ bool ContentCommand(String Command){Subscribe=1; NIndex= (*ReceivedString).indexOf(Command); return NIndex>=0;}
+ int ExtractNumber(int TagLength,uint8_t NumberDigits){NIndex+=TagLength; return (*ReceivedString).substring(NIndex+1,NIndex+NumberDigits+1).toInt();}
+ TerminalDriverObjectF(String *ReceivedString){
+    TerminalDriverObjectF::InstanceCounter+=1;
+    this->ReceivedString=ReceivedString; NIndex=-1; Subscribe=false;}
 };
 const String TerminalDriverObjectF::SyncCommand="SYNC";
-const int TerminalDriverObjectF::InstanceCounter=0;
-const int TerminalDriverObjectF::CountInvalidCommand=0;
+int TerminalDriverObjectF::InstanceCounter=0;
+int TerminalDriverObjectF::CountInvalidCommand=0;
 #endif
