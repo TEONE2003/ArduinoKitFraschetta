bdb2365a430a8d0d3720a8e6c6c78ab04d662f4f
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-01-24 10:42:04 +0100

    aggiornati gli esempi del bluetooth

--- examples/BluetoothFraschetta examples/BluetoothFraschetta_example/BluetoothFraschetta_example.ino
+++ examples/BluetoothFraschetta examples/BluetoothFraschetta_example/BluetoothFraschetta_example.ino
@@ -3,7 +3,9 @@ BluetoothF B(2,3,9600);
 void setup(){pinMode(13,OUTPUT);}
 void loop(){
 B.ReceiveString();
+if(B.Readible()){
 if(B.ReadString()=="a"){digitalWrite(13,HIGH);}
 else if(B.ReadString()=="b"){digitalWrite(13,LOW);}
 else if(B.ReadString()!=""){B.SendLn("INVALID COMMAND: ["+B.ReadString()+"]");}
+}
 }
