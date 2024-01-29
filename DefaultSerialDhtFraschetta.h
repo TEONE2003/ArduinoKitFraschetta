73db962da061290fd826bd2c86cbc6b0110afb2b
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-01-29 12:09:18 +0100

    conformati a TerminalDriver

--- DefaultSerialDhtFraschetta.h
+++ DefaultSerialDhtFraschetta.h
@@ -4,7 +4,7 @@
 #include "DefaultSerialFraschetta.h"
 class DefaultSerialDhtF:DefaultSerialF,public BasicSerialDhtF{
 protected: void Send(String s)override{DefaultSerialF::SendLn(s);}
-public: DefaultSerialDhtF(String Tag,uint8_t Pin,uint8_t Type,String *ReceivedString):BasicSerialDhtF(Tag,Pin,Type,ReceivedString),DefaultSerialF(){}
+public: DefaultSerialDhtF(String Tag,uint8_t Pin,uint8_t Type,String *ReceivedString,bool *Readable):BasicSerialDhtF(Tag,Pin,Type,ReceivedString,Readable),DefaultSerialF(){}
 void Begin(long Baud=9600){DefaultSerialF::Begin(Baud); BasicSerialDhtF::Begin();}
 };
 #endif
--- SerialDhtFraschetta.h
+++ SerialDhtFraschetta.h
@@ -4,7 +4,7 @@
 #include "SerialFraschetta.h"
 class SerialDhtF:SerialF,public BasicSerialDhtF{
 protected: void Send(String s)override{SerialF::SendLn(s);}
-public: SerialDhtF(String Tag,uint8_t Pin,uint8_t Type,uint8_t Rx=2,uint8_t Tx=3,String *ReceivedString):BasicSerialDhtF(Tag,Pin,Type,ReceivedString),SerialF(Rx,Tx){}
+public: SerialDhtF(String Tag,uint8_t Pin,uint8_t Type,uint8_t Rx=2,uint8_t Tx=3,String *ReceivedString,bool *Readable):BasicSerialDhtF(Tag,Pin,Type,ReceivedString,Readable),SerialF(Rx,Tx){}
 void Begin(long Baud=9600){SerialF::SetBaudRate(Baud); BasicSerialDhtF::Begin();}
 }
 #endif
