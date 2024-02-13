5d492d3ebfb6ca5129a13b02552e71058e3cfab5
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 14:29:14 +0100

    Update SerialPhotocellDuskToDawnFraschetta.h

--- SerialPhotocellDuskToDawnFraschetta.h
+++ SerialPhotocellDuskToDawnFraschetta.h
@@ -5,7 +5,7 @@
 class SerialPhotocellDuskToDawnF:public BasicTerminalPhotocellDuskToDawnF,SerialF{
 protected: void Send(String s)override{SerialF::SendLn(s);}
 public:
- SerialPhotocellDuskToDawnF(String *ReceivedString,bool *Readable,String Tag,uint8_t PinPhotoresistor,uint16_t TurnOnThreshold,uint16_t TurnOffThreshold,uint64_t Delay,UnitOfTime Unit,uint8_t PinRele,DigitalType ReleType,uint8_t Rx=2,uint8_t Tx=3):BasicTerminalPhotocellDuskToDawnF(Tag,PinPhotoresistor,TurnOnThreshold,TurnOffThreshold,Delay,Unit,PinRele,ReleType,ReceivedString,Readable),SerialF(Rx,Tx){}
+ SerialPhotocellDuskToDawnF(String *ReceivedString,bool *Readable,String Tag,uint8_t PinPhotoresistor,uint16_t TurnOnThreshold,uint16_t TurnOffThreshold,uint64_t Delay,UnitOfTime Unit,uint8_t PinRele,DigitalType ReleType,uint8_t Rx=2,uint8_t Tx=3):BasicTerminalPhotocellDuskToDawnF(ReceivedString,Readable,Tag,PinPhotoresistor,TurnOnThreshold,TurnOffThreshold,Delay,Unit,PinRele,ReleType),SerialF(Rx,Tx){}
  void Begin(long Baud=9600){SerialF::SetBaudRate(Baud); BasicTerminalPhotocellDuskToDawnF::Begin();}
 };
 #endif
