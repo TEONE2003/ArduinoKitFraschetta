3cb8a4ee89c7e97eeec59c5226be7a63d100b8e2
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 15:10:36 +0100

    Update DefaultSerialServoEntranceFraschetta_example.ino

--- examples/DefaultSerialServoEntranceFraschetta_example/DefaultSerialServoEntranceFraschetta_example.ino
+++ examples/DefaultSerialServoEntranceFraschetta_example/DefaultSerialServoEntranceFraschetta_example.ino
@@ -1,7 +1,7 @@
 #include "DefaultSerialServoEntranceFraschetta.h"
 #include "DefaultBluetoothFraschetta.h"
 DefaultBluetoothF B(9600);
-DefaultSerialServoEntranceF S("S",2,180,0,3,10,9600,B.GetStringAddress());
+DefaultSerialServoEntranceF S(B.GetStringAddress(),B.GetReadableAddress(),"S",2,180,0,3,10,9600);
 void setup(){S.MovementStatusCheck();}
 void loop(){
 B.ReceiveString();
