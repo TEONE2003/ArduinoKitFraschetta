a4d7c935fd541848106e87f9394c114999628b54
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 15:05:36 +0100

    Update BluetoothFraschetta_example.ino

--- examples/BluetoothFraschetta examples/BluetoothFraschetta_example/BluetoothFraschetta_example.ino
+++ examples/BluetoothFraschetta examples/BluetoothFraschetta_example/BluetoothFraschetta_example.ino
@@ -6,6 +6,5 @@ B.ReceiveString();
 if(B.Readable()){
 if(B.ReadString()=="a"){digitalWrite(13,HIGH);}
 else if(B.ReadString()=="b"){digitalWrite(13,LOW);}
-else if(B.ReadString()!=""){B.SendLn("INVALID COMMAND: ["+B.ReadString()+"]");}
 }
 }
