f4fb23f284518dd9bf4f7f5f165e172075f447ce
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 13:52:29 +0100

    Update DefaultSerial1DhtFraschetta.h

--- DefaultSerial1DhtFraschetta.h
+++ DefaultSerial1DhtFraschetta.h
@@ -5,7 +5,7 @@
 #include "DefaultSerial1Fraschetta.h"
 class DefaultSerial1DhtF:DefaultSerial1F,public BasicSerialDhtF{
 protected: void Send(String s)override{DefaultSerial1F::SendLn(s);}
-public: DefaultSerial1DhtF(String Tag,uint8_t Pin,uint8_t Type,String *ReceivedString,bool *Readable):BasicSerialDhtF(Tag,Pin,Type,ReceivedString,Readable),DefaultSerial1F(){}
+public: DefaultSerial1DhtF(String *ReceivedString,bool *Readable,String Tag,uint8_t Pin,uint8_t Type):BasicSerialDhtF(Tag,Pin,Type,ReceivedString,Readable),DefaultSerial1F(){}
 void Begin(long Baud=9600){DefaultSerial1F::SetBaudRate(Baud); BasicSerialDhtF::Begin();}
 };
 #endif
