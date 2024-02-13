e0f5f27b0f5268388ff470e8ff9152e49fccbc05
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 15:14:29 +0100

    Update SerialRelayFraschetta_example.ino

--- examples/SerialRelayFraschetta_example/SerialRelayFraschetta_example.ino
+++ examples/SerialRelayFraschetta_example/SerialRelayFraschetta_example.ino
@@ -1,7 +1,7 @@
 #include "SerialRelayFraschetta.h"
 #include "BluetoothFraschetta.h"
 BluetoothF B(2,3,9600);
-SerialRelayF LivingRoomLight(B.GetStringAddress,B.GetReadableAddress,"LRL",13,NormalLogic,SaveStatus,2,3,9600);
+SerialRelayF LivingRoomLight(B.GetStringAddress(),B.GetReadableAddress(),"LRL",13,NormalLogic,SaveStatus,2,3,9600);
 void setup(){LivingRoomLight.Begin();}
 void loop(){
  B.ReceiveString();
