08a8da3b2a64590d13343c2a7dec5ecd70083474
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 15:11:05 +0100

    Update DefaultSerialThermostatFraschetta_example.ino

--- examples/DefaultSerialThermostatFraschetta_example/DefaultSerialThermostatFraschetta_example.ino
+++ examples/DefaultSerialThermostatFraschetta_example/DefaultSerialThermostatFraschetta_example.ino
@@ -1,7 +1,7 @@
 #include "DefaultSerialThermostatFraschetta.h"
 #include "DefaultBluetoothFraschetta.h"
 DefaultBluetoothF B(9600);
-DefaultSerialThermostatF T("T",13,NormalLogic,Heating,9600);
+DefaultSerialThermostatF T(B.GetStringAddress(),B.GetReadableAddress(),"T",13,NormalLogic,Heating,9600);
 void setup(){T.Begin();}
 void loop(){
 B.ReceiveString();
