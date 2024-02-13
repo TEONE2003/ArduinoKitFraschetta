9f737ba2bb83254b15bbb3865549a7c8d1648211
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 14:01:19 +0100

    Update SerialThermostatFraschetta.h

--- SerialThermostatFraschetta.h
+++ SerialThermostatFraschetta.h
@@ -5,7 +5,7 @@
 class SerialThermostatF:public BasicTerminalThermostatF,SerialF{
 protected: void Send(String s)override{SerialF::SendLn(s);}
 public:
- SerialThermostatF(String Tag,uint8_t PinRele,DigitalType ReleType=NormalLogic,ThermostatType TypeT=Heating,uint8_t Rx=2,uint8_t Tx=3,String *ReceivedString,bool *Readable):BasicTerminalThermostatF(Tag,PinRele,ReleType,TypeT,ReceivedString,Readable),SerialF(Rx,Tx){}
+ SerialThermostatF(String *ReceivedString,bool *Readable,String Tag,uint8_t PinRele,DigitalType ReleType=NormalLogic,ThermostatType TypeT=Heating,uint8_t Rx=2,uint8_t Tx=3):BasicTerminalThermostatF(Tag,PinRele,ReleType,TypeT,ReceivedString,Readable),SerialF(Rx,Tx){}
  void Begin(long Baud=9600){SerialF::SetBaudRate(Baud); BasicTerminalThermostatF::Begin();}
 };
 #endif
