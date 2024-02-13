0f2d5b09c61ee147777b5f265c9929707619324a
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 14:00:47 +0100

    Update SerialServoEntranceFraschetta.h

--- SerialServoEntranceFraschetta.h
+++ SerialServoEntranceFraschetta.h
@@ -5,8 +5,8 @@
 class SerialServoEntranceF:SerialF,public BasicTerminalServoEntranceF{
 protected: void Send(String s)override{SerialF::SendLn(s);}
 public:
-    SerialServoEntranceF(String Tag,uint8_t Pin,uint16_t OpeningPosition,uint16_t ClosingPosition,
-     uint16_t AddressMotionStatusPosition,uint16_t MovementDelayInMilliseconds,uint8_t Rx=2,uint8_t Tx=3,String *ReceivedString,bool *Readable):
+    SerialServoEntranceF(String *ReceivedString,bool *Readable,String Tag,uint8_t Pin,uint16_t OpeningPosition,uint16_t ClosingPosition,
+     uint16_t AddressMotionStatusPosition,uint16_t MovementDelayInMilliseconds,uint8_t Rx=2,uint8_t Tx=3):
      BasicTerminalServoEntranceF(Tag,Pin,OpeningPosition,ClosingPosition,AddressMotionStatusPosition,MovementDelayInMilliseconds,ReceivedString,Readable),
      SerialF(Rx,Tx){}
     void Begin(long Baud=9600){SerialF::SetBaudRate(Baud); BasicTerminalServoEntranceF::Begin();}
