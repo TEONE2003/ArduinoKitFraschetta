f29594a709e3ed0565e72f6888418ef3d82dd8d4
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 14:27:48 +0100

    Update DefaultSerialRelayFraschetta.h

--- DefaultSerialRelayFraschetta.h
+++ DefaultSerialRelayFraschetta.h
@@ -5,7 +5,7 @@
 class DefaultSerialRelayF:DefaultSerialF,public BasicTerminalRelayF{
 protected: void Send(String s)override{DefaultSerialF::SendLn(s);}
 public:
- DefaultSerialRelayF(String *ReceivedString,bool *Readable,String Tag,uint8_t Pin,DigitalType Type=NormalLogic,MemorizeStatus Save=DoNotSaveStatus):BasicTerminalRelayF(Tag,Pin,Type,Save,ReceivedString,Readable),DefaultSerialF(){}
+ DefaultSerialRelayF(String *ReceivedString,bool *Readable,String Tag,uint8_t Pin,DigitalType Type=NormalLogic,MemorizeStatus Save=DoNotSaveStatus):BasicTerminalRelayF(ReceivedString,Readable,Tag,Pin,Type,Save),DefaultSerialF(){}
  void Begin(long Baud=9600){DefaultSerialF::SetBaudRate(Baud); BasicTerminalRelayF::Begin();}
 };
 #endif
