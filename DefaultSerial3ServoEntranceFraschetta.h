7491b7136894947def2feef2c319c490876dc11b
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 14:24:37 +0100

    Update DefaultSerial3ServoEntranceFraschetta.h

--- DefaultSerial3ServoEntranceFraschetta.h
+++ DefaultSerial3ServoEntranceFraschetta.h
@@ -8,7 +8,7 @@ protected: void Send(String s)override{DefaultSerial3F:SendLn(s);}
 public:
     DefaultSerial3ServoEntranceF(String *ReceivedString,bool *Readable,String Tag,uint8_t Pin,uint16_t OpeningPosition,uint16_t ClosingPosition,
      uint16_t AddressMotionStatusPosition,uint16_t MovementDelayInMilliseconds):
-     BasicTerminalServoEntranceF(Tag,Pin,OpeningPosition,ClosingPosition,AddressMotionStatusPosition,MovementDelayInMilliseconds,ReceivedString,Readable),
+     BasicTerminalServoEntranceF(ReceivedString,Readable,Tag,Pin,OpeningPosition,ClosingPosition,AddressMotionStatusPosition,MovementDelayInMilliseconds),
      DefaultSerial3F(){}
     void Begin(long Baud=9600){DefaultSerial3F::SetBaudRate(Baud); BasicTerminalServoEntranceF::Begin();}
 };
