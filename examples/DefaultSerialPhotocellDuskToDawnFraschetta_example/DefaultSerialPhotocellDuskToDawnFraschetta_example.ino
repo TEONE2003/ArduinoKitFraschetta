4586f3cb4fd4e1f76cfee32885c410a47054849f
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 15:09:29 +0100

    Update DefaultSerialPhotocellDuskToDawnFraschetta_example.ino

--- examples/DefaultSerialPhotocellDuskToDawnFraschetta_example/DefaultSerialPhotocellDuskToDawnFraschetta_example.ino
+++ examples/DefaultSerialPhotocellDuskToDawnFraschetta_example/DefaultSerialPhotocellDuskToDawnFraschetta_example.ino
@@ -1,7 +1,7 @@
 #include "DefaultSerialPhotocellDuskToDawnFraschetta.h"
 #include "DefaultBluetoothFraschetta.h"
 DefaultBluetoothF B(9600);
-DefaultSerialPhotocellDuskToDawnF P("P",A0,800,200,1,Seconds,13,NormalLogic,9600,B.GetStringAddress());
+DefaultSerialPhotocellDuskToDawnF P(B.GetStringAddress(),B.GetReadableAddress(),"P",A0,800,200,1,Seconds,13,NormalLogic,9600);
 void setup(){P.Begin();}
 void loop(){
 B.ReceiveString();
