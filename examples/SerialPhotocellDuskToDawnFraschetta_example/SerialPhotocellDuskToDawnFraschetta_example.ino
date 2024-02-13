f810540af6bd1a9eba073cebee05a65e8a6bdb65
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 15:14:09 +0100

    Update SerialPhotocellDuskToDawnFraschetta_example.ino

--- examples/SerialPhotocellDuskToDawnFraschetta_example/SerialPhotocellDuskToDawnFraschetta_example.ino
+++ examples/SerialPhotocellDuskToDawnFraschetta_example/SerialPhotocellDuskToDawnFraschetta_example.ino
@@ -1,10 +1,9 @@
 #include "SerialPhotocellDuskToDawnFraschetta.h"
 #include "BluetoothFraschetta.h"
-SerialPhotocellDuskToDawnF P("P",A0,800,200,1,Seconds,13,NormalLogic,2,3,9600);
 BluetoothF B(2,3,9600);
+SerialPhotocellDuskToDawnF P(B.GetStringAddress(),B.GetReadableAddress(),"P",A0,800,200,1,Seconds,13,NormalLogic,2,3,9600);
 void setup(){}
 void loop(){
 B.ReceiveString();
-P.SerialPhotocellDuskToDawnFunction(B.ReadString());
-if(P.InvalidCommand()){B.SendLn("INVALID COMMAND: ["+B.ReadString()+']');}
+P.SerialPhotocellDuskToDawnFunction();
 }
