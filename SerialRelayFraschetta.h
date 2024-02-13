c46d3d91677d18508fe52f54500e438c3ea05ade
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 14:00:22 +0100

    Update SerialRelayFraschetta.h

--- SerialRelayFraschetta.h
+++ SerialRelayFraschetta.h
@@ -4,7 +4,7 @@
 #include "SerialFraschetta.h"
 class SerialRelayF:SerialF,public BasicTerminalRelayF{
 protected: void Send(String s)override{SerialF::SendLn(s);}
-public: SerialRelayF(String Tag,uint8_t Pin,DigitalType Type=NormalLogic,MemorizeStatus Save=DoNotSaveStatus,uint8_t Rx=2,uint8_t Tx=3,String *ReceivedString,bool *Readable):BasicTerminalRelayF(Tag,Pin,Type,Save,ReceivedString,Readable),SerialF(Rx,Tx){}
+public: SerialRelayF(String *ReceivedString,bool *Readable,String Tag,uint8_t Pin,DigitalType Type=NormalLogic,MemorizeStatus Save=DoNotSaveStatus,uint8_t Rx=2,uint8_t Tx=3):BasicTerminalRelayF(Tag,Pin,Type,Save,ReceivedString,Readable),SerialF(Rx,Tx){}
 void Begin(long Baud=9600){SerialF::SetBaudRate(Baud); BasicTerminalRelayF::Begin();}
 };
 #endif
