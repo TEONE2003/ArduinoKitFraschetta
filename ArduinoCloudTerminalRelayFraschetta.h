29ebbf2cc3e4389cdf68d812803aa85a9efc7b8a
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-13 19:34:44 +0100

    Create ArduinoCloudTerminalRelayFraschetta.h

---
+++ ArduinoCloudTerminalRelayFraschetta.h
@@ -0,0 +1,18 @@
+#define ESP32
+//#define ESP8266
+#if defined(ESP8266) || defined(ESP32)
+#ifndef ArduinoCloudTerminalRelayFraschetta_h
+#define ArduinoCloudTerminalRelayFraschetta_h
+#include <BasicTerminalRelayFraschetta.h>
+class ArduinoCloudTerminalRelayFraschetta:BasicTerminalRelayF{
+private:
+    
+public:
+    ArduinoCloudTerminalRelayFraschetta(),BasicTerminalRelayF();
+    
+};
+
+#endif
+#else
+#error This library requires an Arduino board with WiFi support.
+#endif
