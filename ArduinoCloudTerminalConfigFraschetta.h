893d0254f974004ec95aa718df309b38db8a84f2
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-18 00:37:11 +0100

    Update ArduinoCloudTerminalConfigFraschetta.h

--- ArduinoCloudTerminalConfigFraschetta.h
+++ ArduinoCloudTerminalConfigFraschetta.h
@@ -1,13 +1,5 @@
-#define ESP32
-//#define ESP8266
 #if defined(ESP8266) || defined(ESP32)
-#ifndef ArduinoCloudTerminalConfigFraschetta_h
-#define ArduinoCloudTerminalConfigFraschetta_h
-template <typename T>
-class ArduinoCloudTerminalConfigFraschetta{
-protected: void Send(String S){ T::*ReceivedString=S;}
-};
-#endif
+protected: void Send(String S){*ClassName::ReceivedString=S;}
 #else
 #error This library requires an Arduino board with WiFi support.
 #endif
