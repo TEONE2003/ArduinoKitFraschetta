d41130be2c68286b41ea97c47e37496e0b836c07
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-19 11:59:39 +0100

    Update ArduinoCloudTerminalRelayFraschetta_example.ino

--- examples/ArduinoCloudTerminalRelayFraschetta_example/ArduinoCloudTerminalRelayFraschetta_example.ino
+++ examples/ArduinoCloudTerminalRelayFraschetta_example/ArduinoCloudTerminalRelayFraschetta_example.ino
@@ -1,8 +1,5 @@
 #include <ArduinoCloudTerminalRelayFraschetta.h>
-void Setup(){
-
-}
-
-void Loop(){
-
-}
+bool Readible=false;
+ArduinoCloudTerminalRelayF R(/*arduino cloud string*/,Readible,"R",13,ReverseLogic,SaveStatus)
+void Setup(){R.Begin();}
+void Loop(){R.FunctionSerialRelay();}
