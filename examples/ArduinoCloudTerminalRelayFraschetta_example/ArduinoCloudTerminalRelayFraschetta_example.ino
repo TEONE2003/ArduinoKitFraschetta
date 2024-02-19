099fc8be6c6624cf86d24b382346d299d45f3401
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-19 12:41:17 +0100

    Update ArduinoCloudTerminalRelayFraschetta_example.ino

--- examples/ArduinoCloudTerminalRelayFraschetta_example/ArduinoCloudTerminalRelayFraschetta_example.ino
+++ examples/ArduinoCloudTerminalRelayFraschetta_example/ArduinoCloudTerminalRelayFraschetta_example.ino
@@ -1,8 +1,8 @@
 #include <ArduinoCloudTerminalRelayFraschetta.h>
 bool Readible=false;
-ArduinoCloudTerminalRelayF R(/*arduino cloud string*/,Readible,"R",13,ReverseLogic,SaveStatus)
-void Setup(){R.Begin();}
-void Loop(){R.FunctionSerialRelay();}
+ArduinoCloudTerminalRelayF R(/*arduino cloud string*/,Readible,"R",13,ReverseLogic);
+void setup(){R.Begin();}
+void loop(){R.FunctionSerialRelay();}
 /*This example does not work because the code must be placed in the program with the configuration of the card.
 In the method in which the string changes you only need to write:
 Readable=true;*/
