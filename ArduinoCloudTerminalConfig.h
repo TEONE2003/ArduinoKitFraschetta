15cff61bf60bfcaa3ca6ec70cabb7cf863a0fd9f
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-02-28 12:10:01 +0100

    Update ArduinoCloudTerminalConfig.h

--- ArduinoCloudTerminalConfig.h
+++ ArduinoCloudTerminalConfig.h
@@ -1,5 +1,5 @@
 #if defined(ARDUINO_UNOWIFIR4) || defined(ESP32)
-protected: void Send(String S){*ClassName::ReceivedString=char(125)+ S + char(125);}
+protected: void Send(String S){*ClassName::ReceivedString=char(123)+ S + char(125);}
 #else
 #error This library requires an Arduino board with WiFi support.
 #endif
