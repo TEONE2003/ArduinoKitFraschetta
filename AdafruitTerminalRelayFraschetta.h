fe3bea7afd7afd528554411147b0ff80644bd750
TEONE2003 <121766894+TEONE2003@users.noreply.github.com>
2024-04-15 12:58:42 +0200

    Update AdafruitTerminalRelayFraschetta.h

--- AdafruitTerminalRelayFraschetta.h
+++ AdafruitTerminalRelayFraschetta.h
@@ -1,9 +1,25 @@
+//#define ARDUINO_UNOWIFIR4
 #if defined(ARDUINO_UNOWIFIR4) || defined(ESP32)
+#ifndef AdafruitTerminalRelayFraschetta_h
+#define AdafruitTerminalRelayFraschetta_h
+
 #include "BasicTerminalRelayFraschetta.h"
 #include "BasicAdafruitWifiFraschetta.h"
 
+class AdafruitTerminalRelayF::BasicTerminalRelayF{
+private:
+ BasicAdafruitWifiF& A;
+public:
+ AdafruitTerminalRelayF(String Tag,uint8_t Pin,DigitalType Type=NormalLogic,MemorizeStatus Save=DoNotSaveStatus):BasicTerminalRelayF(nullptr,nullptr,Tag,Pin,Type,Save){
+  A = BasicAdafruitWifiF::GetInstance();
+  ReceivedString = &A.ReceivedString;
+  Readable = &A.Readable;
+}
+
+};
 
 
+#endif
 #else
 #error This library requires an Arduino board with WiFi support.
 #endif
